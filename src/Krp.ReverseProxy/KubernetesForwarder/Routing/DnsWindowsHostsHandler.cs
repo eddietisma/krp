bf0eb8eab7ab0246fe9a730ce9e9bcaf05c8c2a1
Eddie Tisma <eddietisma@users.noreply.github.com>
2025-04-29 01:18:05 +0200

    Refactoring: Folders

--- src/Krp.ReverseProxy/KubernetesForwarder/Routing/DnsWindowsHostsHandler.cs
+++ src/Krp.ReverseProxy/KubernetesForwarder/Routing/DnsWindowsHostsHandler.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Krp.KubernetesForwarder.Dns;
+namespace Krp.KubernetesForwarder.Routing;
 
 public interface IDnsHandler
 {
