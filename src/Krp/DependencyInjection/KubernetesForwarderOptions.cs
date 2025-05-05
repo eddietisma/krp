bcc7a8e4c5955d44451d5eb31c0dc2b2868a8957
Eddie Tisma <eddietisma@users.noreply.github.com>
2025-05-05 21:27:51 +0200

    Add support for HTTP endpoints

--- src/Krp/DependencyInjection/KubernetesForwarderOptions.cs
+++ src/Krp/DependencyInjection/KubernetesForwarderOptions.cs
@@ -1,4 +1,4 @@
-﻿using Krp.KubernetesForwarder.Models;
+﻿using Krp.KubernetesForwarder.Endpoints;
 using System.Collections.Generic;
 
 namespace Krp.DependencyInjection;
@@ -6,4 +6,5 @@ namespace Krp.DependencyInjection;
 public class KubernetesForwarderOptions
 {
     public List<KrpEndpoint> Endpoints { get; set; } = new();
+    public List<KrpHttpEndpoint> HttpEndpoints { get; set; } = new();
 }
---
+++ src/Krp/KubernetesForwarder/Routing/DnsLookupOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace Krp.KubernetesForwarder.Routing;
+
+public class DnsLookupOptions
+{
+    /// <summary>
+    /// DNS server.
+    /// </summary>
+    public string Nameserver { get; set; }
+}
