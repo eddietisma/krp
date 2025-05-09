04133149f0b199038ff494f66ec239d550b4f248
Eddie Tisma <eddietisma@users.noreply.github.com>
2025-05-09 20:55:39 +0200

    Fix issues when running in docker

--- src/Krp/KubernetesForwarder/PortForward/PortChecker.cs
+++ src/Krp/KubernetesForwarder/PortForward/PortChecker.cs
@@ -1,5 +1,7 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Net.NetworkInformation;
+using System.Net.Sockets;
 using System.Threading;
 
 namespace Krp.KubernetesForwarder.PortForward;
@@ -10,6 +12,28 @@ public static class PortChecker
 
     public static bool TryIsPortAvailable(int port)
     {
+        if (Environment.GetEnvironmentVariable("DOTNET_RUNNING_IN_CONTAINER") == "true")
+        {
+            var ipAddress = System.Net.Dns.GetHostAddresses("host.docker.internal")
+                .FirstOrDefault(a => a.AddressFamily == AddressFamily.InterNetwork);
+
+            // Opening the TCP listener to check if it its available is not thread-safe.
+            lock (_lock)
+            {
+                try
+                {
+                    var tcpListener = new TcpListener(ipAddress, port);
+                    tcpListener.Start();
+                    tcpListener.Stop();
+                    return true;
+                }
+                catch (SocketException)
+                {
+                    return false;
+                }
+            }
+        }
+
         var tcpConnInfoArray = IPGlobalProperties.GetIPGlobalProperties().GetActiveTcpListeners();
         return tcpConnInfoArray.All(x => x.Port != port);
     }
