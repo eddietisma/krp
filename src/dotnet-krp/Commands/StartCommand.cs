6dba3a65996d11fb5fe49aeb036f3bc535110c0c
Eddie Tisma <eddietisma@users.noreply.github.com>
2025-05-11 19:04:23 +0200

    Add dotnet tool

---
+++ src/dotnet-krp/Commands/StartCommand.cs
@@ -0,0 +1,87 @@
+﻿using Krp.DependencyInjection;
+using Krp.KubernetesForwarder.Dns;
+using McMaster.Extensions.CommandLineUtils;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Krp.Tool.Commands;
+
+[Command(Name = "", OptionsComparison = StringComparison.InvariantCultureIgnoreCase)]
+public class StartCommand
+{
+    [Option("--no-discovery|-nd", Description = "Disable automatic endpoint discovery")]
+    public bool NoDiscovery { get; init; } = false;
+
+    [Option("--nameservers|-n <NAMESERVERS>", Description = "Comma-separated list of DNS servers")]
+    public string Nameservers { get; init; } = "8.8.8.8";
+
+    [Option("--forwarder|-f <FORWARDER>", Description = "Connection method: tcp, http, or hybrid")]
+    [AllowedValues("tcp", "http", "hybrid", IgnoreCase = true)]
+    public string Forwarder { get; init; } = "hybrid";
+
+    [Option("--routing|-r <ROUTING>", Description = "Routing method (currently only 'hosts')")]
+    [AllowedValues("hosts", IgnoreCase = true)]
+    public string Routing { get; init; } = "hosts";
+
+    public async Task<int> OnExecuteAsync(CommandLineApplication _, CancellationToken ct)
+    {
+        var builder = Host.CreateDefaultBuilder()
+            .ConfigureServices((context, services) =>
+            {
+                var builder = services.AddKubernetesForwarder(context.Configuration)
+                    .UseHttpEndpoint(5000, "gateway-api.qa.hsb.se", "/meetings")
+                    .UseEndpoint(0, 80, "asgnmntattest", "assignment-attest-attestorder-grpcserver-api")
+                    .UseDnsLookup(options =>
+                    {
+                        options.Nameserver = Nameservers;
+                    });
+
+                switch (Routing)
+                {
+                    case "hosts":
+                        builder.UseRouting(DnsOptions.WindowsHostsFile);
+                        break;
+                }
+
+                switch (Forwarder)
+                {
+                    case "http":
+                        builder.UseHttpForwarder();
+                        break;
+                    case "tcp":
+                        builder.UseTcpForwarder(options =>
+                        {
+                            options.ListenAddress = IPAddress.Any;
+                            options.ListenPort = 80;
+
+                        });
+                        break;
+                    case "hybrid":
+                        builder.UseTcpWithHttpForwarder(options =>
+                        {
+                            options.ListenAddress = IPAddress.Any;
+                            options.ListenPort = 80;
+                        });
+                        break;
+                }
+                
+                if (!NoDiscovery)
+                {
+                    builder.UseEndpointExplorer(options =>
+                    {
+                        //options.Filter = [
+                        //    "namespace/meetings/*",
+                        //    "namespace/*/service/person*",
+                        //];
+                        options.RefreshInterval = TimeSpan.FromHours(1);
+                    });
+                }
+            });
+
+        await builder.Build().RunAsync(ct);
+        return 0;
+    }
+}
---
+++ src/dotnet-krp/Program.cs
@@ -0,0 +1,13 @@
+﻿using Krp.Tool.Commands;
+using Microsoft.Extensions.Hosting;
+using System.Threading.Tasks;
+
+namespace Krp.Tool;
+
+public class Program
+{
+    public async static Task Main(string[] args)
+    {
+        await Host.CreateDefaultBuilder(args).RunCommandLineApplicationAsync<StartCommand>(args);
+    }
+}
