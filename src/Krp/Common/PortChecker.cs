7e8cdf35c99213eb58376c168fcd9dcb148229ff
Eddie Tisma <eddietisma@users.noreply.github.com>
2025-05-13 00:51:30 +0200

    Fix issues when running in docker

--- src/Krp/Common/PortChecker.cs
+++ src/Krp/Common/PortChecker.cs
@@ -2,35 +2,25 @@
 using System.Linq;
 using System.Net.NetworkInformation;
 using System.Net.Sockets;
-using System.Threading;
 
 namespace Krp.Common;
 
 public static class PortChecker
 {
-    private static readonly Lock _lock = new Lock();
-
     public static bool TryIsPortAvailable(int port)
     {
         if (Environment.GetEnvironmentVariable("DOTNET_RUNNING_IN_CONTAINER") == "true")
         {
-            var ipAddress = System.Net.Dns.GetHostAddresses("host.docker.internal")
-                .FirstOrDefault(a => a.AddressFamily == AddressFamily.InterNetwork);
-
-            // Opening the TCP listener to check if it its available is not thread-safe.
-            lock (_lock)
+            try
             {
-                try
-                {
-                    var tcpListener = new TcpListener(ipAddress, port);
-                    tcpListener.Start();
-                    tcpListener.Stop();
-                    return true;
-                }
-                catch (SocketException)
-                {
-                    return false;
-                }
+                using var client = new TcpClient();
+                client.Connect("host.docker.internal", port);
+                
+                return !client.Connected;
+            }
+            catch (SocketException)
+            {
+                return true;
             }
         }
 
