803237bca6b59038356e7ed5b4cad1c57563474b
Eddie Tisma <eddietisma@users.noreply.github.com>
2025-05-13 22:34:59 +0200

    Refactoring: Folder structure

--- src/Krp/KubernetesForwarder/Dns/DnsWindowsHostsOptions.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Krp.KubernetesForwarder.Dns;
-
-public class DnsWindowsHostsOptions
-{
-    /// <summary>
-    /// Path to HOSTS file.
-    /// </summary>
-    public string Path { get; set; }
-}
