de04890e3ec6a975b713889645a9f22c4c6b9a7f
Eddie Tisma <eddietisma@users.noreply.github.com>
2025-06-01 22:48:02 +0200

    Add pipelines using buildcharts

--- test/Krp.Tests1/Test1.cs
+++ test/Krp.Tests1/Test1.cs
@@ -1,4 +1,4 @@
-﻿namespace Krp.Tests;
+﻿namespace Krp.Tests1;
 
 [TestClass]
 public sealed class Test1
@@ -6,8 +6,10 @@ public sealed class Test1
     [TestMethod]
     public void TestMethod1()
     {
+#pragma warning disable MSTEST0025
 #pragma warning disable MSTEST0032
-        Assert.IsTrue(true, "This test should always pass.");
+        Assert.IsTrue(false, "This test should always pass.");
 #pragma warning restore MSTEST0032
+#pragma warning restore MSTEST0025
     }
 }
