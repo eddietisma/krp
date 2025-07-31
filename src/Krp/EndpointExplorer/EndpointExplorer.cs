ac4998fffefcc0303eade99593ff790af756b6de
Eddie Tisma <eddietisma@users.noreply.github.com>
2025-07-31 03:06:08 +0200

    Improve thread safety for endpoint explorer

--- src/Krp/EndpointExplorer/EndpointExplorer.cs
+++ src/Krp/EndpointExplorer/EndpointExplorer.cs
@@ -1,9 +1,11 @@
 ﻿using k8s;
 using k8s.Models;
+using Krp.Common;
 using Krp.Endpoints;
 using Krp.Endpoints.Models;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,6 +21,7 @@ public class EndpointExplorer
     private readonly EndpointManager _endpointManager;
     private readonly ILogger<EndpointExplorer> _logger;
     private readonly List<Regex> _compiledFilters = [];
+    private readonly SemaphoreSlim _lock = new SemaphoreSlim(1, 1);
 
     public EndpointExplorer(IOptions<EndpointExplorerOptions> options, EndpointManager endpointManager, ILogger<EndpointExplorer> logger)
     {
@@ -34,49 +37,59 @@ public class EndpointExplorer
     
     public async Task DiscoverEndpointsAsync(CancellationToken ct)
     {
-        _logger.LogInformation("Discovering endpoints...");
+        // Prevent running multiple endpoints discovery simultaneously.
+        await _lock.WaitAsync(ct);
 
-        var config = KubernetesClientConfiguration.BuildConfigFromConfigFile();
-        var client = new Kubernetes(config);
-        var services = new ConcurrentBag<V1Service>();
-
-        var namespaces = await client.ListNamespaceAsync(cancellationToken: ct);
-        
-        await Parallel.ForEachAsync(namespaces.Items, new ParallelOptions { MaxDegreeOfParallelism = 100, CancellationToken = ct }, async (ns, cancellationToken) =>
+        try
         {
-            var result = await FetchServicesAsync(ns.Metadata.Name, client, cancellationToken);
+            _logger.LogInformation("Discovering endpoints...");
 
-            foreach (var svc in result)
-            {
-                services.Add(svc);
-            }
-        });
+            var config = KubernetesClientConfiguration.BuildConfigFromConfigFile();
+            var client = new Kubernetes(config);
+            var services = new ConcurrentBag<V1Service>();
 
-        var filteredServices = services
-            .Where(x => x.Spec?.Ports != null)
-            .Where(x =>
+            var namespaces = await client.ListNamespaceAsync(cancellationToken: ct);
+
+            await Parallel.ForEachAsync(namespaces.Items, new ParallelOptions { MaxDegreeOfParallelism = 100, CancellationToken = ct }, async (ns, cancellationToken) =>
             {
-                var fullName = $"namespace/{x.Namespace()}/service/{x.Name()}";
-                return _compiledFilters.Count == 0 || _compiledFilters.Any(regex => regex.IsMatch(fullName));
+                var result = await FetchServicesAsync(ns.Metadata.Name, client, cancellationToken);
+
+                foreach (var svc in result)
+                {
+                    services.Add(svc);
+                }
             });
 
-        foreach (var service in filteredServices)
-        {
-            foreach (var port in service.Spec.Ports)
-            {
-                var endpoint = new KubernetesEndpoint
+            var filteredServices = services
+                .Where(x => x.Spec?.Ports != null)
+                .Where(x =>
                 {
-                    LocalPort = 0,
-                    Namespace = service.Namespace(),
-                    RemotePort = port.Port,
-                    Resource = $"service/{service.Name()}",
-                };
+                    var fullName = $"namespace/{x.Namespace()}/service/{x.Name()}";
+                    return _compiledFilters.Count == 0 || _compiledFilters.Any(regex => regex.IsMatch(fullName));
+                });
 
-                _endpointManager.AddEndpoint(endpoint);
+            foreach (var service in filteredServices)
+            {
+                foreach (var port in service.Spec.Ports)
+                {
+                    var endpoint = new KubernetesEndpoint
+                    {
+                        LocalPort = 0,
+                        Namespace = service.Namespace(),
+                        RemotePort = port.Port,
+                        Resource = $"service/{service.Name()}",
+                    };
+
+                    _endpointManager.AddEndpoint(endpoint);
+                }
             }
-        }
 
-        _endpointManager.TriggerEndPointsChangedEvent();
+            _endpointManager.TriggerEndPointsChangedEvent();
+        }
+        finally
+        {
+            _lock.Release();
+        }
     }
 
     private async Task<IEnumerable<V1Service>> FetchServicesAsync(string ns, Kubernetes client, CancellationToken ct)
