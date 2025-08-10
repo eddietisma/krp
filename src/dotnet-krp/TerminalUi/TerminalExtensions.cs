ef08879bbbe1641fa8416a2de792a6d41ad71433
Eddie Tisma <eddietisma@users.noreply.github.com>
2025-08-10 02:45:17 +0200

    Add logging table

---
+++ src/dotnet-krp/TerminalUi/TerminalExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Krp.Tool.TerminalUi;
+
+
+public static class TerminalExtensions
+{
+    public static string GetLevelMarkup(this LogLevel level) => level switch
+    {
+        LogLevel.Trace => "[grey]TRA[/]",
+        LogLevel.Debug => "[grey]DBG[/]",
+        LogLevel.Information => "[green]INF[/]",
+        LogLevel.Warning => "[yellow]WRN[/]",
+        LogLevel.Error => "[red]ERR[/]",
+        LogLevel.Critical => "[red]CRI[/]",
+        _ => level.ToString().ToUpperInvariant()
+    };
+}
