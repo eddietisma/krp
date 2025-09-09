cad5fa004326d6e507b930d8b55d69f4aee82150
Eddie Tisma <eddietisma@users.noreply.github.com>
2025-09-10 00:13:29 +0200

    Optimization: PortChecker now uses caching

--- src/Krp/Common/PortChecker.cs
+++ src/Krp/Common/PortChecker.cs
@@ -2,29 +2,37 @@
 using System.Linq;
 using System.Net.NetworkInformation;
 using System.Net.Sockets;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace Krp.Common;
 
 public static class PortChecker
 {
+    private static readonly MemoryCache _cache = new(new MemoryCacheOptions());
+    private static readonly TimeSpan _ttl = TimeSpan.FromMilliseconds(500);
+
     public static bool TryIsPortAvailable(int port)
     {
-        if (Environment.GetEnvironmentVariable("DOTNET_RUNNING_IN_CONTAINER") == "true")
+        return _cache.GetOrCreate(port, entry =>
         {
-            try
-            {
-                using var client = new TcpClient();
-                client.Connect("host.docker.internal", port);
-                
-                return !client.Connected;
-            }
-            catch (SocketException)
-            {
-                return true;
-            }
-        }
+            entry.AbsoluteExpirationRelativeToNow = _ttl;
 
-        var tcpConnInfoArray = IPGlobalProperties.GetIPGlobalProperties().GetActiveTcpListeners();
-        return tcpConnInfoArray.All(x => x.Port != port);
+            if (Environment.GetEnvironmentVariable("DOTNET_RUNNING_IN_CONTAINER") == "true")
+            {
+                try
+                {
+                    using var client = new TcpClient();
+                    client.Connect("host.docker.internal", port);
+                    return !client.Connected;
+                }
+                catch (SocketException)
+                {
+                    return true;
+                }
+            }
+
+            var tcpConnInfoArray = IPGlobalProperties.GetIPGlobalProperties().GetActiveTcpListeners();
+            return tcpConnInfoArray.All(x => x.Port != port);
+        });
     }
 }
