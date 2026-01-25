1a726f4535d623fb8c50ba30ece0c5963728d63c
Eddie Tisma <eddietisma@users.noreply.github.com>
2026-01-25 13:51:26 +0100

    Fix Update-KrpTool.ps1

--- Update-KrpTool.ps1
+++ Update-KrpTool.ps1
@@ -1,3 +1,4 @@
+buildcharts generate
 dotnet tool uninstall --global dotnet-krp
-$env:VERSION="9.9.9"; $env:COMMIT="abc123"; docker buildx bake 
-dotnet tool install --global dotnet-krp --add-source ./output/pack
+docker buildx bake --file .buildcharts/docker-bake.hcl --no-cache --set *.args.VERSION="9.9.9" --set *.args.COMMIT=$(git rev-parse HEAD) nuget
+dotnet tool install --global dotnet-krp --add-source ./.buildcharts/output/nuget
