942d69cb9f75972ef6cd0b90f5fbf3e00dafceb0
Eddie Tisma <eddietisma@users.noreply.github.com>
2026-05-17 01:17:36 +0200

    Handle unrecognized root commands

--- src/dotnet-krp/Program.cs
+++ src/dotnet-krp/Program.cs
@@ -1,8 +1,11 @@
 using Krp.Https.DependencyInjection;
-using Krp.Tool.Help;
 using Krp.Tool.Commands;
+using Krp.Tool.Help;
 using McMaster.Extensions.CommandLineUtils;
 using Microsoft.Extensions.Hosting;
+using System;
+using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Krp.Tool;
@@ -20,14 +23,32 @@ public class Program
                 })
                 .RunCommandLineApplicationAsync<RootCommand>(args, app =>
                 {
+                    if (args.Length > 0 && !args.Any(IsInfoOption))
+                    {
+                        app.Out = TextWriter.Null;
+                    }
+
                     app.HelpTextGenerator = new KrpHelpTextGenerator();
                     app.ExtendedHelpText = @"Environment variables:
   KRP_HOSTS                       Override path to hosts file";
                 });
         }
-        catch (UnrecognizedCommandParsingException)
+        catch (UnrecognizedCommandParsingException ex)
         {
+            var message = ex.Command.Name == "krp" && !args[0].StartsWith('-')
+               ? $"unknown command '{string.Join(" ", args)}'"
+               : ex.Message;
+
+            await Console.Error.WriteLineAsync($"krp: {message}");
+            await Console.Error.WriteLineAsync();
+            await Console.Error.WriteLineAsync("Run 'krp --help' for more information");
+
             return 1;
         }
+    }
+
+    private static bool IsInfoOption(string arg)
+    {
+        return arg is "--help" or "-h" or "-?" or "--version" or "-v";
     }
 }
